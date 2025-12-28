66c652b7492b970636ca38b0b442d46514592929
Maia <diogomaia2003@gmail.com>
2025-12-28 02:43:28 +0000

    update shadcn theme colors

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,10 @@ import { Button } from "./components/ui/button";
 
 const App = () => {
   return (
-    <>
+    <div>
       <h1 className="text-xl font-bold text-red-600">Hello world</h1>
       <Button>Click Me</Button>
-    </>
+    </div>
   );
 };
 
