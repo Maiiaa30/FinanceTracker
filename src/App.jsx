164632f2fb1d5ba74fa26f5f5660fe988fa4cbad
Maiaa30 <diogomaia2003@gmail.com>
2025-12-28 01:59:26 +0000

    add shadcn setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
+import { Button } from "./components/ui/button";
+
 const App = () => {
-  return <h1 className="text-xl font-bold text-red-600">Hello world</h1>;
+  return (
+    <>
+      <h1 className="text-xl font-bold text-red-600">Hello world</h1>
+      <Button>Click Me</Button>
+    </>
+  );
 };
 
 export default App;
