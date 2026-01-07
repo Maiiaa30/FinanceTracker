fb34a48267c99aa7f61e720f9739f40fce22c91e
Maia <diogomaia2003@gmail.com>
2026-01-07 23:09:17 +0000

    move local storage keys to is own file and ensure access token is attached on all api requests

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,5 +1,14 @@
 import axios from "axios";
 
+import { LOCAL_STORAGE_ACCESS_TOKEN_KEY } from "@/constants/localStorage";
+
 export const api = axios.create({
   baseURL: "https://fullstackclub-finance-dashboard-api.onrender.com/api",
+});
+
+api.interceptors.request.use((request) => {
+  const accessToken = localStorage.getItem(LOCAL_STORAGE_ACCESS_TOKEN_KEY);
+  if (!accessToken) return request;
+  request.headers.Authorization = `Bearer ${accessToken}`;
+  return request;
 });
