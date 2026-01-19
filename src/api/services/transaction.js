5323bd6a59091670cdb98379034cb756bd75ee69
Maia <diogomaia2003@gmail.com>
2026-01-19 19:09:11 +0000

    add get all function to transactions service

--- src/api/services/transaction.js
+++ src/api/services/transaction.js
@@ -1,8 +1,18 @@
+import queryString from "query-string";
+
 import { protectedApi } from "@/lib/axios";
 
 export const TransactionService = {
   create: async (input) => {
     const response = await protectedApi.post("/transactions/me", input);
+    return response.data;
+  },
+  getAll: async (input) => {
+    const queryParams = queryString.stringify({
+      from: input.from,
+      to: input.to,
+    });
+    const response = await protectedApi.get(`/transactions/me?${queryParams}`);
     return response.data;
   },
 };
